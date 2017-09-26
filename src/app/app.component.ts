81d0093e0e038dd43fbad8b46236ccec994c23fd
dittoPDX <steventran325@gmail.com>
2017-09-25 19:25:07 -0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnChanges, SimpleChanges, Input, Output, EventEmitter, Renderer, ViewChild } from '@angular/core';
+
+@Component
+({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent
+{
+	text:string = "12345";
+	options:any = {maxLines: 1000, printMargin: false, fontSize: "20px"};
+
+	//Need to send AJAX post request for express server to handle
+	onChange(event)
+	{
+		console.log(event);
+	}
+	
+	toggled: boolean = false;
+	toggled2: boolean = false;
+
+	//Handle user click event for menu
+	menuToggle()
+	{
+		this.toggled = !this.toggled;
+		//console.log(this.toggled);
+	}
+
+	//Handle user click event for languages
+	langToggle()
+	{
+		this.toggled2 = !this.toggled2;
+		//console.log(this.toggled2);
+	}
+}
